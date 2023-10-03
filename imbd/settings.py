a310352aa56fe56cba8b584a4ac419aba9c8d765
vaibhavchandrana <vaibhavrana8900@gmail.com>
2023-10-03 12:40:42 +0530

    updating again

--- imbd/settings.py
+++ imbd/settings.py
@@ -45,6 +45,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.contrib.staticfiles.middleware.StaticFilesMiddleware',
 
 ]
 
